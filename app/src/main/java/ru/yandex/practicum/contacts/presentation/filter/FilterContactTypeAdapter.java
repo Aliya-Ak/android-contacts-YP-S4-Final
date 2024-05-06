142cb1654f68986690e04bf8c57eec3c10b41d5e
Aliya-Ak <HelloAliya@yandex.ru>
2024-05-06 20:47:31 +0300

    Спринт 4 Проектная работа №2

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactTypeAdapter.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactTypeAdapter.java
@@ -26,7 +26,6 @@ public class FilterContactTypeAdapter extends RecyclerView.Adapter<FilterContact
 
     private final AsyncListDiffer<FilterContactTypeUi> differ = new AsyncListDiffer<>(
             new AdapterListUpdateCallback(this),
-            //new AsyncDifferConfig.Builder<>(new ListDiffCallback()).build()
             new AsyncDifferConfig.Builder<>(new BaseListDiffCallback<FilterContactTypeUi>()).build()
     );
 
